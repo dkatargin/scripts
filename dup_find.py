394d330c74a81ed21ff988a7b696a2ea377fb96f
Dmitry Katargin <d.katargin@corp.mail.ru>
2023-02-05 22:16:02 +0300

    black fixes; add .gitignore;

--- dup_find.py
+++ dup_find.py
@@ -7,23 +7,32 @@ import sys
 
 files_dict = {}
 duped = []
-ignored = [".DS_Store", ".directory", "desktop.ini", "folder.htt", "no-data.txt", "message_1.json", "dovecot-uidlist"]
+ignored_files = [
+    ".DS_Store",
+    ".directory",
+    "desktop.ini",
+    "folder.htt",
+    "no-data.txt",
+    "message_1.json",
+    "dovecot-uidlist",
+]
+ignored_dir = "@eaDir"
 
 
-def fast_check(file_path, file_size):
-    '''
+def fast_check(file_path, file_size) -> str:
+    """
     Check files equal by names and size.
-    '''
+    """
     return f"{os.path.basename(file_path)}_{file_size}"
 
 
-def deep_check(file_path, file_size):
-    '''
+def deep_check(file_path, file_size) -> str:
+    """
     Check files equal by md5 hash  and size.
-    '''
+    """
     hash_md5 = hashlib.md5()
 
-    fobj = open(os.path.join(file_path),'rb')
+    fobj = open(os.path.join(file_path), "rb")
     chunk_data = fobj.read(4096)
     fobj.close()
 
@@ -32,15 +41,15 @@ def deep_check(file_path, file_size):
     return f"{hash_md5.hexdigest()}_{file_size}"
 
 
-def make_xml():
-     # generate xml struct
+def xml_output():
+    # generate xml struct
     print('<?xml version="1.0" encoding="UTF-8"?>')
     print("<root>")
     for dkey in duped:
         f_name = "_".join(dkey.split("_")[:-1])
         f_size = dkey.split("_")[-1]
         print(f'\t<files data_key="{f_name}" size={f_size}>')
-        for fpath in files_dict.get(dkey):
+        for fpath in files_dict.get(dkey, []):
             print(f'\t\t<file path="{fpath}"/>')
         print(f"\t</files>")
     print("</root>")
@@ -52,13 +61,13 @@ def main(start_path, scan_type):
     else:
         check_key_getter = fast_check
 
-    for path,_,files in os.walk(start_path):
+    for path, _, files in os.walk(start_path):
         for f in files:
-            if f in ignored:
+            if f in ignored_files:
                 continue
 
-            file_path = os.path.join(path,f)
-            if "@eaDir" in file_path:
+            file_path = os.path.join(path, f)
+            if ignored_dir in file_path:
                 continue
             try:
                 size = os.path.getsize(file_path)
@@ -76,14 +85,14 @@ def main(start_path, scan_type):
             else:
                 files_dict[check_key].append(file_path)
                 duped.append(check_key)
-    make_xml()
+    xml_output()
     return
 
 
 if __name__ == "__main__":
-    scan_type = "fast"
-    
-    if len(sys.argv) >= 2:
-        if len(sys.argv) >= 3:
-            scan_type = sys.argv[2]
-        main(sys.argv[1], scan_type)
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    else:
+        print(
+            "Wrong number of arguments. Use: python3 dup_find.py <start_path> <scan_type>. scan_type can be 'fast' or 'deep'"
+        )
